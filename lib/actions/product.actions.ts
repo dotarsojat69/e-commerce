3c68f4a210cba6b0bcd9e3c7ce8b9a995113cfd3
dotarsojat69 <tdeath457@gmail.com>
2024-10-25 00:13:43 +0700

    feat: implement add to cart

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,20 +1,20 @@
-'use server'
+"use server";
 
-import db from "@/db/drizzle"
-import { products } from "@/db/schema"
-import { desc } from "drizzle-orm"
-import { eq } from 'drizzle-orm/sql'
+import db from "@/db/drizzle";
+import { products } from "@/db/schema";
+import { desc } from "drizzle-orm";
+import { eq } from "drizzle-orm/sql";
 
 export async function getLatestProduct() {
-    const data = await db.query.products.findMany({
-        orderBy: [desc(products.createdAt)],
-        limit: 4,
-    })
-    return data
+  const data = await db.query.products.findMany({
+    orderBy: [desc(products.createdAt)],
+    limit: 4,
+  });
+  return data;
 }
 
 export async function getProductBySlug(slug: string) {
-    return await db.query.products.findFirst({
-      where: eq(products.slug, slug),
-    })
-  }
+  return await db.query.products.findFirst({
+    where: eq(products.slug, slug),
+  });
+}
