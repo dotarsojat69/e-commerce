eb8105eb876d87a806245bfb8e4938219781a64e
dotarsojat69 <tdeath457@gmail.com>
2024-10-24 17:31:18 +0700

    deploy on vercel

---
+++ app/(root)/page.tsx
@@ -0,0 +1,15 @@
+import ProductList from "@/components/shared/product/product-list"
+import { getLatestProduct } from "@/lib/actions/product.actions"
+
+export default async function Home() {
+  const latestProducts = await getLatestProduct()
+  
+  return (
+    <div className="space-y-8">
+      <ProductList title= "Newest Arrivals" data={latestProducts} />
+    </div>
+  )
+}
+
+
+
---
+++ lib/constant/index.ts
@@ -0,0 +1,4 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Test-Ecommerce'
+   export const APP_DESCRIPTION =
+     process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+     'Test E-commerce build with Next.js, Postgres, Shadcn & should be Nest.js'
